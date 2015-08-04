41a93ac5135985011eb427e9d1519e6ca9d56984
melisoner2006 <melisoner2006>
2015-08-04 13:42:03 -0400

    Add ListView layout Create fakeforecast data

--- src/main/java/com/example/android/sunshine/MainActivityFragment.java
+++ src/main/java/com/example/android/sunshine/MainActivityFragment.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,6 +21,19 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/64",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Rainy - 64/51",
+                "Fri - Foggy - 79/46",
+                "Sat - Sunny - 70/46",
+                "Sun - Sunny - 76/70",
+        };
+
+        ArrayList<String> weekForcast = new ArrayList<String>(Arrays.asList(forecastArray));
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
